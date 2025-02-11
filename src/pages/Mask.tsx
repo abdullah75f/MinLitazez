32492014babec79d9a05349458574adbf908d1a5
Abdullah.F <abdullah75farid@gmail.com>
2025-02-11 15:16:45 +0300

    mask section

--- src/pages/Mask.tsx
+++ src/pages/Mask.tsx
@@ -1,4 +1,4 @@
-import guyyaMin1 from "../assets/images/guuyyaMin1.png";
+import guuyyaMin1 from "../assets/images/guuyyaMin1.png";
 
 const Mask = () => {
   return (
@@ -14,7 +14,7 @@ const Mask = () => {
 
         <div className="p-8 bg-blue-100 bg-opacity-95 rounded-2xl shadow-2xl">
           <img
-            src={guyyaMin1}
+            src={guuyyaMin1}
             alt="Guyya Facemask"
             className="w-full h-auto rounded-lg mb-6 shadow-lg"
           />
